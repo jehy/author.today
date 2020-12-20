98bc36f6caff291375b43c9a5d655c55d8fb4b6a
Jehy <fate@jehy.ru>
2020-12-20 22:58:33 +0300

    add support for cookie file

--- download.ts
+++ download.ts
@@ -3,20 +3,20 @@ import puppeteer from 'puppeteer-extra';
 // add stealth plugin and use defaults (all evasion techniques)
 
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
-import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker'
-
+import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker';
 
 puppeteer.use(AdblockerPlugin()).use(StealthPlugin());
 
 import * as fs from 'fs';
 import {join} from 'path';
+import {Page, SetCookie} from "puppeteer";
 
+function randomIntFromInterval(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-async function readPage(page, id) {
-
-    console.log(`Downloading part ${id}`);
-    await page.waitForTimeout(5000);
-    const text = await page.evaluate(() => {
+async function getPageText(page: Page): Promise<string> {
+    return page.evaluate(() => {
         async function copyText(selector) {
             const copyText = document.querySelector(selector);
             //copyText.select();
@@ -26,32 +26,47 @@ async function readPage(page, id) {
 
         return copyText("#reader");
     });
+}
+
+async function readPage(page: Page, id: number) {
+    console.log(`Downloading page ${id}`);
+    await page.waitForTimeout(4000 + randomIntFromInterval(100, 2000));
+    const text = await getPageText(page);
     fs.writeFileSync(join(__dirname, `/tmp/${id}.txt`), text, {encoding: "utf-8"});
     console.log(text);
     const linkHandlers = await page.$x("//a[contains(text(), '→')]");
 
     if (linkHandlers.length > 0) {
         await linkHandlers[0].click();
-        await readPage(page, id + 1);
+        return true;
     } else {
         // TODO somehow add check that book is finished. Simply check if that's the last chapter?
-        throw new Error("Link not found")
+        return false;
     }
 }
 
-async function getBook(id) {
+async function getBook(id: string, cookieFile: string) {
     const dir = join(__dirname, '/tmp/');
-    if(!fs.existsSync(dir)){
+    if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
     }
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
+    const cookies:Array<SetCookie> = cookieFile ? JSON.parse(fs.readFileSync(cookieFile, 'utf-8')): [];
+    console.log('Cookies:')
+    console.log(cookies);
+    await page.setCookie(...cookies);
     await page.goto(`https://author.today/reader/${id}`);
-    await readPage(page, 1);
+    let pageFound = true;
+    let pageId = 1;
+    while (pageFound) {
+        pageFound = await readPage(page, pageId);
+        pageId++;
+    }
     await browser.close();
 }
 
-getBook(process.argv[2])
+getBook(process.argv[2], process.argv[3])
     .then(() => process.exit(0))
     .catch((err) => {
         console.log(err);
--- glue.ts
+++ glue.ts
@@ -1,16 +1,14 @@
 import * as fs from 'fs';
 import {join} from 'path';
 
-let data ='';
+let data = '';
 const final = join(__dirname, `/tmp/final.txt`);
-for(let i=0;i<1000; i++)
-{
-    const filename = join(__dirname, `/tmp/${i+1}.txt`);
-    if(!fs.existsSync(filename))
-    {
+for (let i = 0; i < 1000; i++) {
+    const filename = join(__dirname, `/tmp/${i + 1}.txt`);
+    if (!fs.existsSync(filename)) {
         fs.writeFileSync(final, data, {encoding: "utf8"});
         break;
     }
     const newText = fs.readFileSync(filename, 'utf-8');
-    data+=newText;
+    data += newText;
 }
