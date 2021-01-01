55265d32826742fd8cf3605a6991fe7e24bfca70
Jehy <fate@jehy.ru>
2021-01-01 15:26:21 +0300

    added title and to to start of book

--- download.ts
+++ download.ts
@@ -5,70 +5,105 @@ import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker';
 
+import * as fs from 'fs';
+import { join } from 'path';
+import { Page, SetCookie } from 'puppeteer';
+
 puppeteer.use(AdblockerPlugin()).use(StealthPlugin());
 
-import * as fs from 'fs';
-import {join} from 'path';
-import {Page, SetCookie} from "puppeteer";
-
 function randomIntFromInterval(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 async function getPageText(page: Page): Promise<string> {
-    return page.evaluate(() => {
-        async function copyText(selector) {
-            const copyText = document.querySelector(selector);
-            //copyText.select();
-            //document.execCommand("Copy");
-            return copyText.innerText;
-        }
+  return page.evaluate(() => {
+    async function copyText(selector) {
+      const element = document.querySelector(selector);
+      // copyText.select();
+      // document.execCommand("Copy");
+      return element.innerText;
+    }
 
-        return copyText("#reader");
-    });
+    return copyText('#reader');
+  });
 }
 
-async function readPage(page: Page, id: number) {
-    console.log(`Downloading page ${id}`);
-    await page.waitForTimeout(4000 + randomIntFromInterval(100, 2000));
-    const text = await getPageText(page);
-    fs.writeFileSync(join(__dirname, `/tmp/${id}.txt`), text, {encoding: "utf-8"});
-    console.log(text);
-    const linkHandlers = await page.$x("//a[contains(text(), '→')]");
+async function readPage(page: Page, id: number, bookDir: string) {
+  console.log(`Downloading page ${id}`);
+  await page.waitForTimeout(4000 + randomIntFromInterval(100, 2000));
+  const text = await getPageText(page);
+  fs.writeFileSync(join(bookDir, `/${id}.txt`), text, { encoding: 'utf-8' });
+  console.log(`${text.substr(0, 100).split('\n').join('')}...`);
+  const nextPageLink = await page.$x("//a[contains(text(), '→')]");
 
-    if (linkHandlers.length > 0) {
-        await linkHandlers[0].click();
-        return true;
+  if (nextPageLink.length > 0) {
+    await nextPageLink[0].click();
+    return true;
+  }
+  // TODO somehow add check that book is finished. Simply check if that's the last chapter?
+  console.log('No next link found, possibly book end');
+  return false;
+}
+
+// Otherwise when using cookies book will be downloaded from last opened page
+async function goToBookStart(page: Page, id: string) {
+  await page.goto(`https://author.today/reader/${id}`);
+  let notFirstPage = true;
+  while (notFirstPage) {
+    // eslint-disable-next-line no-await-in-loop
+    const nextPageLink = await page.$x("//a[contains(text(), '←')]");
+
+    if (nextPageLink.length > 0) {
+      console.log('Not book start, going to previous page');
+      // eslint-disable-next-line no-await-in-loop
+      await page.waitForTimeout(4000 + randomIntFromInterval(100, 2000));
+      // eslint-disable-next-line no-await-in-loop
+      await nextPageLink[0].click();
     } else {
-        // TODO somehow add check that book is finished. Simply check if that's the last chapter?
-        return false;
+      notFirstPage = false;
+      return;
     }
+  }
+}
+
+async function getBookTitle(page: Page, id: string) {
+  const url = `https://author.today/work/${id}`;
+  await page.goto(url);
+  const bookTitle = await page.title();
+  return bookTitle.replace(' - читать книгу в онлайн-библиотеке', '');
 }
 
 async function getBook(id: string, cookieFile: string) {
-    const dir = join(__dirname, '/tmp/');
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
-    }
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-    const cookies:Array<SetCookie> = cookieFile ? JSON.parse(fs.readFileSync(cookieFile, 'utf-8')): [];
-    console.log('Cookies:')
-    console.log(cookies);
-    await page.setCookie(...cookies);
-    await page.goto(`https://author.today/reader/${id}`);
-    let pageFound = true;
-    let pageId = 1;
-    while (pageFound) {
-        pageFound = await readPage(page, pageId);
-        pageId++;
-    }
-    await browser.close();
+  const dir = join(__dirname, '/tmp/');
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  const cookies:Array<SetCookie> = cookieFile ? JSON.parse(fs.readFileSync(cookieFile, 'utf-8')) : [];
+  console.log('Cookies:');
+  console.log(cookies);
+  await page.setCookie(...cookies);
+  const bookTitle = await getBookTitle(page, id);
+  await goToBookStart(page, id);
+  await page.goto(`https://author.today/reader/${id}`);
+  let pageFound = true;
+  let pageId = 1;
+  const bookDir = join(dir, `/${bookTitle}`);
+  if (!fs.existsSync(bookDir)) {
+    fs.mkdirSync(bookDir);
+  }
+  while (pageFound) {
+    // eslint-disable-next-line no-await-in-loop
+    pageFound = await readPage(page, pageId, bookDir);
+    pageId++;
+  }
+  await browser.close();
 }
 
 getBook(process.argv[2], process.argv[3])
-    .then(() => process.exit(0))
-    .catch((err) => {
-        console.log(err);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
--- glue.ts
+++ glue.ts
@@ -1,14 +1,19 @@
 import * as fs from 'fs';
-import {join} from 'path';
+import { join } from 'path';
 
-let data = '';
-const final = join(__dirname, `/tmp/final.txt`);
-for (let i = 0; i < 1000; i++) {
-    const filename = join(__dirname, `/tmp/${i + 1}.txt`);
+const books = fs.readdirSync(join(__dirname, '/tmp'));
+books.forEach((bookName) => {
+  console.log(`Gluing book ${bookName}`);
+  let data = '';
+  const final = join(__dirname, `/tmp/${bookName}/${bookName}.txt`);
+  for (let i = 0; i < 1000; i++) {
+    const filename = join(__dirname, `tmp/${bookName}/${i + 1}.txt`);
     if (!fs.existsSync(filename)) {
-        fs.writeFileSync(final, data, {encoding: "utf8"});
-        break;
+      fs.writeFileSync(final, data, { encoding: 'utf8' });
+      break;
     }
+    console.log(`Found page ${i + 1}`);
     const newText = fs.readFileSync(filename, 'utf-8');
     data += newText;
-}
+  }
+});
